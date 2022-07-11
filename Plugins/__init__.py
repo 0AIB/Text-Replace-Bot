07322469627d382eee5c29250812cb30d831c258
Lx <89601803+lx575@users.noreply.github.com>
2022-07-11 17:22:30 +0530

    Add files via upload

---
+++ Plugins/__init__.py
@@ -0,0 +1,21 @@
+import os
+from os import environ
+
+# Your Auto Forward Channel ID's
+FROM_CHANNELS = set(int(x)
+                    for x in os.environ.get("FROM_CHANNELS", "").split())
+TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "").split())
+
+# required to Movies information
+OMDB_KEY = environ.get("OMDB_KEY", "")
+
+RE1TXT = "@Latest_Movies_Reborn"
+RE2TXT = os.environ.get("RE2TXT", "@JESSEVERSE")
+RE3TXT = os.environ.get("RE3TXT", "⚠️ Uploaded By @HQFilms4U")
+RE4TXT = os.environ.get("RE4TXT", "@BollyWeBHolly")
+RE5TXT = os.environ.get("RE5TXT", "@File_Movies_Uploaded")
+RE6TXT = os.environ.get("RE6TXT", "@RunningMovies")
+HQTXT = os.environ.get("RE2TXT", "@IMDbFilms4U")
+
+# text you want to replace with text above.
+REPLACED = "Hollywood_0980"
---
+++ Plugins/replace.py
@@ -0,0 +1,45 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import asyncio
+from pyrogram import filters
+from bot import autoforward
+from config import Config
+from Plugins import RE1TXT, RE2TXT, RE3TXT, RE4TXT, RE5TXT, RE6TXT, REPLACED
+
+# =
+usercaption_position = Config.CAPTION_POSITION
+caption_position = usercaption_position.lower()
+caption_text = Config.CAPTION_TEXT
+
+@autoforward.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
+async def editing(bot, message):
+      try:
+         media = message.document or message.video or message.audio or message.photo
+         caption_text = Config.CAPTION_TEXT
+      except:
+         caption_text = ""
+         pass 
+      if (message.document or message.video or message.photo or message.audio): 
+          if message.caption:                        
+             file_caption = f"{message.caption}"                
+          else:
+             fname = media.file_name
+             filename = fname.replace("_", ".")
+             file_caption = f"`{filename}`"  
+              
+      try:
+          if caption_position == "bottom":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id, 
+                 message_id = message.message_id,
+                 caption = file_caption.replace(RE1TXT, REPLACED).replace(RE2TXT, REPLACED).replace(RE3TXT, REPLACED).replace(RE4TXT, REPLACED).replace(RE5TXT, REPLACED).replace(RE6TXT, REPLACED) + "\n\n" + caption_text,      
+                 parse_mode = "markdown"
+             )
+      except:
+          pass
+              
+                   
+      
