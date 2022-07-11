20183a174951a89f879783d3e24425c4ebe08a2f
Lx <89601803+lx575@users.noreply.github.com>
2022-07-11 19:00:40 +0530

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -9,7 +9,7 @@ TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "").split())
 # required to Movies information
 OMDB_KEY = environ.get("OMDB_KEY", "")
 
-RE1TXT = os.environ.get("RE2TXT", "@Username"
+RE1TXT = os.environ.get("RE1TXT", "@Username"
 RE2TXT = os.environ.get("RE2TXT", "@JESSEVERSE")
 RE3TXT = os.environ.get("RE3TXT", "⚠️ Uploaded By @HQFilms4U")
 RE4TXT = os.environ.get("RE4TXT", "@BollyWeBHolly")
