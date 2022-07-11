088c4abb8d6ed06568e455d29c7b0d3a3196d1ea
Lx <89601803+lx575@users.noreply.github.com>
2022-07-11 18:44:37 +0530

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -9,13 +9,12 @@ TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "").split())
 # required to Movies information
 OMDB_KEY = environ.get("OMDB_KEY", "")
 
-RE1TXT = "@Latest_Movies_Reborn"
+RE1TXT = os.environ.get("RE2TXT", "@Username"
 RE2TXT = os.environ.get("RE2TXT", "@JESSEVERSE")
 RE3TXT = os.environ.get("RE3TXT", "⚠️ Uploaded By @HQFilms4U")
 RE4TXT = os.environ.get("RE4TXT", "@BollyWeBHolly")
 RE5TXT = os.environ.get("RE5TXT", "@File_Movies_Uploaded")
 RE6TXT = os.environ.get("RE6TXT", "@RunningMovies")
-HQTXT = os.environ.get("RE2TXT", "@IMDbFilms4U")
 
 # text you want to replace with text above.
-REPLACED = "Hollywood_0980"
+REPLACED = os.environ.get("REPLACED", "@Hollywood_0980")
