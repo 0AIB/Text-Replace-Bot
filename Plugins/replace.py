e7699484f1cd34f81f923d091cb889c0ec932415
Lx <89601803+lx575@users.noreply.github.com>
2022-07-13 23:34:58 +0530

    Update replace.py
    Signed-off-by: Lx <89601803+lx575@users.noreply.github.com>

--- Plugins/replace.py
+++ Plugins/replace.py
@@ -13,15 +13,16 @@ usercaption_position = Config.CAPTION_POSITION
 caption_position = usercaption_position.lower()
 caption_text = Config.CAPTION_TEXT
 
+
 @autoforward.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
 async def editing(bot, message):
       try:
-         media = message.document or message.video or message.audio or message.photo
+         media = message.document or message.video or message.audio
          caption_text = Config.CAPTION_TEXT
       except:
          caption_text = ""
          pass 
-      if (message.document or message.video or message.photo or message.audio): 
+      if (message.document or message.video or message.audio): 
           if message.caption:                        
              file_caption = f"**{message.caption}**"                
           else:
