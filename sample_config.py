15d49e50a80aef58e123e36e675b9ab4cec95fbd
Lx <89601803+lx575@users.noreply.github.com>
2022-08-09 14:44:46 +0530

    Update sample_config.py
    Signed-off-by: Lx <89601803+lx575@users.noreply.github.com>

--- sample_config.py
+++ sample_config.py
@@ -14,6 +14,4 @@ class Config(object):
 
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
 
-    MEDIA_FORWARD_ID = int(os.environ.get("MEDIA_FORWARD_ID", 12345))
-
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
