9222c41fd4e8b78dd59a05c53b8a3b312196b752
Lx <89601803+lx575@users.noreply.github.com>
2022-08-09 14:43:31 +0530

    🔥
    Signed-off-by: Lx <89601803+lx575@users.noreply.github.com>

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,4 +1,3 @@
-import os
 from os import environ
 
 # Your Auto Forward Channel ID's
@@ -9,14 +8,14 @@ TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "").split())
 # required to Movies information
 OMDB_KEY = environ.get("OMDB_KEY", "")
 
-RE1TXT = os.environ.get("RE1TXT", "@Username")
-RE2TXT = os.environ.get("RE2TXT", "@JESSEVERSE")
+RE1TXT = os.environ.get("RE1TXT", "@Username1")
+RE2TXT = os.environ.get("RE2TXT", "@Username1")
 RE3TXT = os.environ.get("RE3TXT", "⚠️ Uploaded By @HQFilms4U")
-RE4TXT = os.environ.get("RE4TXT", "@BollyWeBHolly")
-RE5TXT = os.environ.get("RE5TXT", "@File_Movies_Uploaded")
-RE6TXT = os.environ.get("RE6TXT", "@RunningMovies")
+RE4TXT = os.environ.get("RE4TXT", "@Username3")
+RE5TXT = os.environ.get("RE5TXT", "@Username4")
+RE6TXT = os.environ.get("RE6TXT", "@Username5")
 
 # text you want to replace with text above.
 REPLACED = os.environ.get("REPLACED", "@Hollywood_0980")
 
-MEDIA_FORWARD_ID = int(os.environ.get("MEDIA_FORWARD_ID"))
+# MEDIA_FORWARD_ID = int(os.environ.get("MEDIA_FORWARD_ID"))
