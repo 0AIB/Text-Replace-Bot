aeff5d335115d688d1ce3607280ede456b7c2c5a
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-05 11:44:24 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ class Translation(object):
       HELP_TEXT = "<u>💡 𝐇𝐞𝐥𝐩</u>\n\n👉 <i>Add me as a Admin in your channel with edit permission</i>\n\n👉 <i>Add your caption & " \
                   " Auto Forward Channel ID's, Replace Caption in heroku Config Var</i>"                       
       ABOUT_TEXT = "📕 𝖠𝖻𝗈𝗎𝗍 𝗆𝖾\n\n○ 𝖬𝗒 𝖭𝖺𝗆𝖾 : Channel Automation\n○ 𝖫𝖺𝗎𝗇𝗀𝗎𝖺𝗀𝖾 : Python\n○ 𝖲𝖾𝗋𝗏𝖾𝗋 Heroku\n○ 𝖵𝖾𝗋𝗌𝗂𝗈𝗇 : 2.0.1\n○ 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : @Subhan011"
-      SOURCE_TEXT = "○ <b> I Am Available Open Source on Github</b>\n\n○ <i>SOURCE</i> : <b><a href='https://github.com/lx575/TG-Text-Replace-Bot'>Channel Automationbot</a></b>"  
+      SOURCE_TEXT = "○ <b> I Am Available Open Source on Github</b>\n\n○ <i>SOURCE</i> : <b><a href='https://github.com/0AIB/Text-Replace-Bot'>Channel Automationbot</a></b>"  
       MEDIA_REPLACE_TEXT ="<b>Follow the steps...</b>\n\n🌀First Send Me A Media That You Need To Edit/Replace The Other One\n🌀Send The Link Of The Media That Will Be Replaced/ " \
                           "Edited\n\nNB: Note both you & the bot must be an admin in the targert channel"
       CURRENT_CAPTION ="🖌️ <b>Auto Caption :</b>{}\n\n📐 <b>Current Position :</b> {}"
